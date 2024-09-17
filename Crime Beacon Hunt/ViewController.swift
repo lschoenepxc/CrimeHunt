e1564e30f22905d29c2a6cda1dc1a270538fd208
LauraSchöne <schoene_l@web.de>
2024-09-17 20:16:23 +0200

    added content on welcome screen

--- Crime Beacon Hunt/ViewController.swift
+++ Crime Beacon Hunt/ViewController.swift
@@ -15,12 +15,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView! // UI element added
     @IBOutlet weak var pageControl: UIPageControl! // UI element added
+    var labelTexts = [String]()
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelTexts = []
+        
+        var text = """
+        Willkommen Detektiv,
+                    
+        In der vergangenen Nacht wurde Professor Mats Klein, ein Dozent des Fachbereichs Medienproduktion, während der Dreharbeiten eines Filmprojekts auf dem Campus ermordet. Die Polizei ist ratlos und braucht nun deine Hilfe.
+                    
+        Wer hat Professor Klein getötet?
+        Wo geschah es, und wie wurde er ermordet?
+        """
+        labelTexts.append(text)
+        
+        text = """
+        Was ist zu tun?
+
+        Als Detektiv musst du sechs versteckte Beacon auf dem Campus finden und die anschließend freigeschalteten Rätsel lösen. Durch das korrekte Lösen dieser Rätsel erhältst du die entscheidenden Indizien, mit denen du in der Akte den Täter, die Tatwaffe und den Tatort identifizieren kannst.
+
+        Nur mit scharfem Verstand und einem klaren Plan wirst du es schaffen, den Mordfall zu lösen.
+        """
+        labelTexts.append(text)
+        
+        text = """
+        Aber Vorsicht…
+
+        das Lösen der Rätsel kann zwar beliebig oft versucht werden, doch jeder Fehlversuch verringert die zu erspielenden Punkte. Der Detektiv, der den Mordfall am schnellsten löst und dabei die meisten Punkte erzielt, wird als Sieger hervorgehen.
+        Bringe also nun die Wahrheit ans Licht – die Gerechtigkeit liegt in deinen Händen!
+        """
+        labelTexts.append(text)
         
         // 1.) scrollView
         scrollView.delegate = self // ViewController should take care if user scrolls (delegation)
@@ -37,15 +67,47 @@ class ViewController: UIViewController {
         let numberOfPages = 3  // amount of content inside the scrollView = pageControl !
         for i in 0..<numberOfPages {
                         
-            let page = UIImageView()
+            //let page = UIImageView()
             let number = i + 1 // because i starts with 0
-            page.image = UIImage(named: "Explanation\(number)") // images in assets e.g. Explanation1
+            
+            let label = UILabel()
+            label.numberOfLines = 0  // Ermöglicht unbegrenzte Zeilen
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textColor = .white
+                    
+            // 4. Text für das Label mit Zeilenumbrüchen
+            label.text = labelTexts[i]
+                    
+            label.font = UIFont.systemFont(ofSize: 18)  // Schriftgröße setzen
+            
+            // Container View für jede Seite
+            let pageView = UIView()
+            pageView.translatesAutoresizingMaskIntoConstraints = false
+            scrollView.addSubview(pageView)
+            
+            // Füge das Label zur Seite hinzu
+            pageView.addSubview(label)
+            
+            // Auto Layout für jede Seite
+            NSLayoutConstraint.activate([
+                pageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                pageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                pageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+                pageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: CGFloat(i) * scrollView.frame.size.width),
+                        
+                // Label-Constraints innerhalb der Seite
+                label.topAnchor.constraint(equalTo: pageView.topAnchor, constant: 20),
+                label.leadingAnchor.constraint(equalTo: pageView.leadingAnchor, constant: 20),
+                label.trailingAnchor.constraint(equalTo: pageView.trailingAnchor, constant: -20),
+                label.bottomAnchor.constraint(lessThanOrEqualTo: pageView.bottomAnchor, constant: -20)
+            ])
             
             // 4 values x/y: top corner left (origin) | width | height)
             // width & height equal scrollView
-            page.frame = CGRect(x: CGFloat(i) * scrollView.frame.size.width, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
-            scrollView.addSubview(page)
+            //label.frame = CGRect(x: CGFloat(i) * scrollView.frame.size.width, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
+            //scrollView.addSubview(label)
         }
+        
         // we need to setup the ContentSize (3 pages side by side)
         scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(numberOfPages), height: scrollView.frame.size.height)
         scrollView.isPagingEnabled = true
